cee8dab0082346f04733a122bece9c03eea14115
damiankrupa1 <krupa5643@gmail.com>
2026-07-15 14:40:42 +0200

    feat: update commit lint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,12 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'refactor', 'docs', 'style', 'test', 'build'],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-max-length': [2, 'always', 72],
+  },
 }
