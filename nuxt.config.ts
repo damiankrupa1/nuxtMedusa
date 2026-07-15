bbbf577231b766f6f33584c51ddd0293a7bd51ec
damiankrupa1 <krupa5643@gmail.com>
2026-07-15 15:19:32 +0200

    feat: add variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page', mode: 'out-in' },
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css', '~/assets/css/tokens.css'],
 
   ui: {
     colorMode: false,
